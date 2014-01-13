4bd5f00d9e48d95ed41bc89f80f59deb2cca093c
Dave Parillo <dave.parillo@gmail.com>
2014-01-12 21:36:14 -0800

    Fixed issue #1. Added language element to VB.xml. Added history dialog box and made several minor enhancements.

---
+++ CodeLadder/History.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class History
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.txtHistory = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.OK_Button.Location = New System.Drawing.Point(428, 425)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 1
+        Me.OK_Button.Text = "OK"
+        '
+        'txtHistory
+        '
+        Me.txtHistory.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtHistory.Location = New System.Drawing.Point(12, 12)
+        Me.txtHistory.Multiline = True
+        Me.txtHistory.Name = "txtHistory"
+        Me.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtHistory.Size = New System.Drawing.Size(488, 397)
+        Me.txtHistory.TabIndex = 2
+        '
+        'History
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 460)
+        Me.Controls.Add(Me.txtHistory)
+        Me.Controls.Add(Me.OK_Button)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "History"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "History"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents txtHistory As System.Windows.Forms.TextBox
+
+End Class
